9d2c79a9e074763c9c8eb00b132c2a7057ef1c91
Chris Achard <chrisachard@gmail.com>
2018-11-26 15:50:05 -0500

    Adds hooks in service worker for install and activate events.  Also shows how to wait for async install event

--- src/sw.js
+++ src/sw.js
@@ -3,4 +3,19 @@ workbox.skipWaiting()
 workbox.clientsClaim()
 
 
+self.addEventListener('install', event => {
+  const asyncInstall = new Promise(resolve => {
+    console.log("Waiting to resolve...")
+    setTimeout(resolve, 5000)    
+  })
+
+  event.waitUntil(asyncInstall)
+})
+
+
+self.addEventListener('activate', event => {
+  console.log("activate")
+})
+
+
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
