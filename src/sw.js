0f7f857539f63ef75bd5bfb516c4c7ca4835f936
Chris Achard <chrisachard@gmail.com>
2018-11-26 16:04:20 -0500

    Cache third party resources by adding a special cache route with workbox

--- src/sw.js
+++ src/sw.js
@@ -2,20 +2,11 @@
 workbox.skipWaiting()
 workbox.clientsClaim()
 
-
-self.addEventListener('install', event => {
-  const asyncInstall = new Promise(resolve => {
-    console.log("Waiting to resolve...")
-    setTimeout(resolve, 5000)    
+workbox.routing.registerRoute(
+  new RegExp('https:.*min\.(css|js)'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'cdn-cache'
   })
-
-  event.waitUntil(asyncInstall)
-})
-
-
-self.addEventListener('activate', event => {
-  console.log("activate")
-})
-
+)
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
